ba4ea2c0f5506cfa63d8bc263e1758b86128779d
star-no <star.no@navercorp.com>
2022-04-14 00:11:23 +0900

    gameStarter에 restart 기능 연동
    하지만 BaseballGameScanner 클래스의 의존성 부분이 마음에 들지 않는다.
    이 클래스도 printer와 같이 restartScanner로 분리해야겠다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,24 @@
 package baseball;
 
+import baseball.printer.BaseballGameRestartCommandLinePrinter;
+import baseball.printer.BaseballGameRestartPrinter;
+import baseball.scanner.BaseballGameMissionUtilsScanner;
+import baseball.scanner.BaseballGameScanner;
+
 public class Application {
+
     public static void main(String[] args) {
-        boolean isFinish = false;
-        while (!isFinish) {
+        BaseballGameRestartPrinter restartPrinter = new BaseballGameRestartCommandLinePrinter();
+        // todo BaseballGameScanner 을 BaseballGameRestartScanner 로 분리할 것
+        BaseballGameScanner gameScanner = new BaseballGameMissionUtilsScanner();
+
+        boolean isRunning = true;
+        while (isRunning) {
             BaseballGameStarter baseballGameStarter = new BaseballGameStarter();
             baseballGameStarter.start();
+
+            restartPrinter.printRestartMessage();
+            isRunning = gameScanner.scanRestartSignal();
         }
     }
 }
--- src/main/java/baseball/BaseballGameStarter.java
+++ src/main/java/baseball/BaseballGameStarter.java
@@ -4,5 +4,6 @@ public class BaseballGameStarter {
 
     public void start() {
         // todo
+        System.out.println("게임 시작되었습니다.");
     }
 }
